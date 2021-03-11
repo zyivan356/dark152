fae47ab01bc6edc3724a7ee4eaf351c0ab1b9211
tyan2D <zyivan356@yandex.ru>
2021-03-11 20:17:19 +0300

    1

--- dark152/urls.py
+++ dark152/urls.py
@@ -1,4 +1,4 @@
-"""dark152 URL Configuration
+"""upvote URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('accounts.urls', 'accounts'))),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
