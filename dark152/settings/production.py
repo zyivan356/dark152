909bc1375b3baef1b2e55d5420a89c67bba324b8
tyan2D <zyivan356@yandex.ru>
2021-03-12 15:50:50 +0300

    1

--- dark152/settings/production.py
+++ dark152/settings/production.py
@@ -78,6 +78,8 @@ TEMPLATES = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 WSGI_APPLICATION = 'dark152.wsgi.application'
 
 
@@ -136,4 +138,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-AUTH_USER_MODEL = 'accounts.MyUser'
