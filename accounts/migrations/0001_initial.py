176f9dc8d3179c686fad892e93f30746866c1c1e
tyan2D <zyivan356@yandex.ru>
2021-03-12 16:48:14 +0300

    1

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('description', models.TextField(blank=True, max_length=255, null=True)),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
                 ('email', models.EmailField(max_length=255, unique=True, verbose_name='email address')),
                 ('username', models.CharField(max_length=30, unique=True, verbose_name='username')),
