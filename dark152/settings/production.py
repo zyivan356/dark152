3e5593cad55bcfea6b11d73f58034bff6b8e2775
tyan2D <zyivan356@yandex.ru>
2021-04-08 00:05:00 +0300

    1

--- dark152/settings/production.py
+++ dark152/settings/production.py
@@ -139,6 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
