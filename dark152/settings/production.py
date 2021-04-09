3afe6241d1c57f65db033f8f94ee9cd8036fc1a9
tyan2D <zyivan356@yandex.ru>
2021-04-09 23:16:26 +0300

    1

--- dark152/settings/production.py
+++ dark152/settings/production.py
@@ -140,6 +140,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -149,6 +150,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_MEDIA_URL = "{}/{}/".format(AWS_URL, AWS_STORAGE_BUCKET_NAME)
 MEDIA_URL = AWS_MEDIA_URL
+
 
 CKEDITOR_CONFIGS = {
    'default': {
